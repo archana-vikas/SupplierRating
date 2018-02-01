8d9bb80d2fdc08aada23ba051a36025c07be8696
Archana Dixit <archanavdixit@yahoo.com>
2018-01-31 19:21:10 -0600

    Supplier page initial

---
+++ DataAccessLayer/SupplierDapperDAO.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DataAccessLayer.Models;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class SupplierDapperDAO
+    {
+        //Connection string to establish connection with sql server//
+        string connectionString = "Data source=LAPTOP-KTJ0MN8C\\SQLEXPRESS; Initial Catalog=SupplierAndUserDB; Integrated Security=SSPI";
+        //Method to get list of Suppliers//
+        public List<Supplier> GetAllSuppliersDAO()
+        {
+                       
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                //To have list of all the suppliers//
+                var result = conn.Query<Supplier>("Select * from Supplier");
+                return result.ToList();
+                
+            }
+                                
+        }
+        //To get Supplier by id//
+        public Supplier GetSupplierById(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var result = conn.Query<Supplier>("Select SupplierId,SupplierName,Service,Rating from Supplier where SupplierId=@SupplierId", new { SupplierId = id }).SingleOrDefault();
+                return result;
+            }
+        }
+
+        //Method to add Supplier in the List//
+        public Supplier AddSupplierDAO(Supplier addSupplierObj)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var query=@"insert into Supplier(SupplierName,Service) values(@SupplierName, @Service);
+                            select cast(scope_identity() as int);";
+                //var result=conn.Execute(query, new { addSupplierObj.SupplierName, addSupplierObj.Service, });
+                var result = conn.Query<int>(query, new { addSupplierObj.SupplierName, addSupplierObj.Service }).Single();
+                addSupplierObj.SupplierId = result;
+                return addSupplierObj;
+            }
+
+        }
+
+        //Update(Edit) Supplier//
+        public bool EditSupplier(Supplier supplierEditObj)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var result = conn.Execute("update Supplier set SupplierName=@SupplierName, service=@Service where SupplierId=" + supplierEditObj.SupplierId, supplierEditObj);
+                if(result>0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        //Deleting the record//
+        public bool DeleteSupplier(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var result = conn.Execute("delete from Supplier where SupplierId=@SupplierId", new { supplierId = id });
+                if(result>0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ SupplierAndUserWebAPI/Controllers/SupplierWebAPIController.cs
@@ -0,0 +1,54 @@
+﻿using DataAccessLayer;
+using System.Web.Http;
+using DataAccessLayer.Models;
+using System.Web.Http.Cors;
+namespace SupplierAndUserWebAPI.Controllers
+{
+    [EnableCors(origins:"*",headers:"*",methods:"*")]
+    public class SupplierController : ApiController
+    {
+        SupplierDapperDAO objSupplier = new SupplierDapperDAO();
+        //Method to get list of all the suppliers//
+        [HttpGet]
+        public IHttpActionResult GetAllSuppliersWebAPI()
+        {
+            var result = objSupplier.GetAllSuppliersDAO();
+            return Ok(result);
+        }
+
+        //Method to get Supplier by Id//
+        [HttpGet]
+        public IHttpActionResult GetSupplierById(int id)
+        {
+            var result = objSupplier.GetSupplierById(id);
+            return Ok(result);
+
+        }
+
+        //Method to add supplier//
+        [HttpPost]
+        public IHttpActionResult AddSupplier(Supplier AddSupplier)
+        {
+            var result = objSupplier.AddSupplierDAO(AddSupplier);
+            return Ok(result);
+        } 
+
+        //Update(Edit) Supplier//
+        [HttpPut]
+        public IHttpActionResult EditSupplier(Supplier supplierObj)
+        {
+            var result = objSupplier.EditSupplier(supplierObj);
+            return Ok(result);
+        }
+
+        //Delete row from Supplier//
+        [HttpDelete]
+        public IHttpActionResult DeleteSupplier(int id)
+        {
+            var result = objSupplier.DeleteSupplier(id);
+            return Ok(result);
+        }
+
+
+    }
+}
